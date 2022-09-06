4edaff39a07b4a2ee6de3b9a16c107fdbe3b171c
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-09-06 17:58:16 +0200

    render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 
 // fetch necessary data for the post with id
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -29,6 +29,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
