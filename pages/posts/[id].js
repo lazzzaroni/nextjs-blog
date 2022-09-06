d66e0d9342f2a7e2de5b2b3b58ab7e0635fca4f9
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-09-06 18:11:35 +0200

    polishing the post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 // fetch necessary data for the post with id
@@ -24,13 +27,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
